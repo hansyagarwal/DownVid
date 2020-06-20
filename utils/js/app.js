187658e1752ba1fdc557fafacd98f090664a787b
aryanag <hansy.agarwal@gmail.com>
2020-06-20 12:16:13 +0530

    url fetch

--- utils/js/app.js
+++ utils/js/app.js
@@ -1,1 +1,11 @@
 console.log("app.js is working!")
+
+const down = document.querySelector('form')
+const search = document.querySelector('input')
+
+down.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const url = search.value;
+
+    console.log(url)
+})
