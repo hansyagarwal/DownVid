4f72e2d127a84f2121466cf0dde327d79877cc3b
aryanag <hansy.agarwal@gmail.com>
2020-06-20 13:07:19 +0530

    ok

--- utils/js/app.js
+++ utils/js/app.js
@@ -2,10 +2,13 @@ console.log("app.js is working!")
 
 const down = document.querySelector('form')
 const search = document.querySelector('input')
+const img = document.querySelector('img')
 
 down.addEventListener('submit',(e)=>{
     e.preventDefault()
     const url = search.value;
+    const vidId = url.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop()
+    img.src = "//img.youtube.com/vi/"+vidId+"/0.jpg"
 
-    console.log(url)
+    console.log(vidId)
 })
