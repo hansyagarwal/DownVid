c76e8f533e76454e55c2f6f48e74146089c9b29e
aryanag <hansy.agarwal@gmail.com>
2020-06-20 08:52:40 +0530

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const mainPage = path.join(__dirname,'/utils')
+const viewsPath = path.join(__dirname,'/utils/views')
+const partialsPath = path.join(__dirname,'/utils/partials')
+
+app.set('view engine', 'hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.urlencoded())
+app.use(express.static(mainPage))
+
+app.get('',(req,res)=>{
+    res.render('index')
+})
+
+app.listen(port,()=>{
+    console.log("Server running on port: " + port)
+})
