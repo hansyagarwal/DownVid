28ca41e617bd486a49286ecd1e62fc9cd9eb3b00
aryanag <hansy.agarwal@gmail.com>
2020-06-20 22:53:00 +0530

    minor change

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 const express = require('express')
 const hbs = require('hbs')
 const path = require('path')
-const down = require('./utils/js/download')
+const down = require('./utils/download')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-const mainPage = path.join(__dirname,'/utils')
-const viewsPath = path.join(__dirname,'/utils/views')
-const partialsPath = path.join(__dirname,'/utils/partials')
+const mainPage = path.join(__dirname,'./utils')
 
-app.set('view engine', 'hbs')
-app.set('views',viewsPath)
-hbs.registerPartials(partialsPath)
-
+app.use(express.urlencoded());
 app.use(express.static(mainPage))
 
-app.get('/',(req,res)=>{
-    res.render('index')
+app.get('',(req,res)=>{
+    res.sendFile(path.join(__dirname + './utils/index.html'))
 })
 
-app.post('/',(req,res)=>{
+app.post('/download',(req,res)=>{
     res.render('index') 
-    const url = req.query.url
-    console.log(url)
+    down(req.body)
 })
 
 app.listen(port,()=>{
