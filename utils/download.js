d529fcdda401d0e1dd7c0b5f517f4fa63f666f87
aryanag <hansy.agarwal@gmail.com>
2020-06-21 09:44:57 +0530

    minor path change

--- utils/download.js
+++ utils/download.js
@@ -2,7 +2,7 @@ const ytdl = require('ytdl-core')
 const fs = require('fs')
 
 const down = (url)=>{
-    ytdl(url).pipe(fs.createWriteStream('video.flv'))
+    ytdl(url).pipe(fs.createWriteStream('Downloads/video.flv'))
 }
 
 module.exports = down
