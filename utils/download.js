daf0d997a6c53f5807d39c5d4516e52ddbdb1f51
aryanag <hansy.agarwal@gmail.com>
2020-07-10 11:21:19 +0530

    minor

--- utils/download.js
+++ utils/download.js
@@ -1,8 +1,12 @@
 const ytdl = require('ytdl-core')
 const fs = require('fs')
 
+const viddir = require('os').homedir();;
+console.log(viddir)
+
 const down = (url)=>{
-    ytdl(url).pipe(fs.createWriteStream('Downloads/video.flv'))
+    ytdl(url).pipe(fs.createWriteStream(viddir+/Downloads/+'vid.flv'))
+    //youtubedl(url).pipe(fs.createWriteStream('myvid.mp4'))
 }
 
 module.exports = down
